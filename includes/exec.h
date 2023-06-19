9bb511aabbe14efaa106072ae1923fc8ddce78a0
Noan Loupe <noloupe@fu-r2-p5.s19.be>
2023-06-19 18:03:08 +0200

    added pwd

--- includes/exec.h
+++ includes/exec.h
@@ -6,7 +6,7 @@
 /*   By: noloupe <noloupe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 14:17:51 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/06/19 15:53:34 by noloupe          ###   ########.fr       */
+/*   Updated: 2023/06/19 17:54:21 by noloupe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	builtin_echo(char **str);
 void	builtin_env(char **str);
 void	builtin_exit(char **str);
 void	builtin_export(char **str);
-void	builtin_pwd(t_env **env);
+void	builtin_pwd(void);
 void	builtin_unset(char **str);
 int		key_check(char *key, char *word);
 
--- srcs/builtins/tester.c
+++ srcs/builtins/tester.c
@@ -6,7 +6,7 @@
 /*   By: noloupe <noloupe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 15:37:54 by noloupe           #+#    #+#             */
-/*   Updated: 2023/06/19 15:53:14 by noloupe          ###   ########.fr       */
+/*   Updated: 2023/06/19 17:54:09 by noloupe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void builtins_tester(char *line)
 		builtin_echo(s_line);
 	// else if (key_check(s_line[0], "cd"))
 	// 	builtin_cd(s_line);
-	// else if (key_check(s_line[0], "pwd"))
-	// 	builtin_pwd(s_line);
+	else if (key_check(s_line[0], "pwd"))
+		builtin_pwd();
 	else if (key_check(s_line[0], "export"))
 		builtin_export(s_line);
 	else if (key_check(s_line[0], "unset"))
