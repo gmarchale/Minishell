02e3e17e6b248c2674ceec20032e8a88166bb679
Gaetan <gaetan.marchale@hotmail.com>
2023-09-05 15:24:52 +0200

    Need to do signals in processes

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,12 @@
 /*   By: gmarchal <gmarchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:48:06 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/09/04 17:43:07 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/09/05 15:22:48 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-/*
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_env	*env;
@@ -55,42 +55,3 @@ int	main(int argc, char **argv, char **envp)
 	free_list(env);
 	return (0);
 }
-*/
-
-
-int	main(int argc, char **argv, char **envp)
-{
-	t_env	*env;
-	char	*line;
-	t_lexlst	*str_input;
-
-	(void)argc;
-	(void)argv;
-	str_input = NULL;
-	shell = malloc(sizeof(t_shell));
-	if (!shell)
-		return (1);
-	if (*envp)
-		env = env_init(envp);
-	else
-		env = create_env();
-	if (!env)
-	{
-		printf("env failed\n");
-		free(shell);
-		exit(1);
-	}
-	shell->env = env;
-	while (1)
-	{
-		line = readline("\e[1;5;96m\U0001f90d Heaven \U0001f90d \u2022\e[0m ");
-		if (line == NULL)
-			return (0); // free plus tard
-		else
-			create_heredoc("lim");
-		free(line);
-	}
-	free_list(env);
-	return (0);
-}
-
--- srcs/signal.c
+++ srcs/signal.c
@@ -6,16 +6,17 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 15:38:43 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/09/04 15:40:04 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/09/05 15:22:34 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	signal_handler_ctrl_c(int sig)
+static void	sig_input(int sig)
 {
 	(void)sig;
 	
+	// change exit value?
 	write(1, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line("", 0);
@@ -27,6 +28,6 @@ void	signal_handler(int input)
 	signal(SIGQUIT, SIG_IGN);
 	if (input == 0)
 	{
-		signal(SIGINT, signal_handler_ctrl_c);
+		signal(SIGINT, &sig_input);
 	}
 }
