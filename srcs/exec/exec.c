6ac825e96e0c27f60332335992fbc135a4e35e89
Gaetan <gaetan.marchale@hotmail.com>
2023-09-10 18:54:09 +0200

    Signaux + Heredoc

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: noloupe <noloupe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 14:11:32 by noloupe           #+#    #+#             */
-/*   Updated: 2023/09/05 18:35:36 by noloupe          ###   ########.fr       */
+/*   Updated: 2023/09/10 16:56:12 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -260,6 +260,7 @@ void	execution(t_cmd *cmdlst)
 {
 	char	**env;
 
+	signal_handler(1);
 	if (!cmdlst)
 		return ;
 	if (is_solo_builtin(cmdlst))
@@ -270,4 +271,5 @@ void	execution(t_cmd *cmdlst)
 		//print_env_arr(env);
 		execute(cmdlst, env);
 	}
+	signal_handler(0);
 }
