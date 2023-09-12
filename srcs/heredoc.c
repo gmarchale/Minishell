817fc76339d92135afdec7643ea443bd51650f8c
Gaetan <gaetan.marchale@hotmail.com>
2023-09-12 16:50:34 +0200

    Norm src/heredoc.c

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:42:13 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/09/12 14:33:38 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/09/12 16:49:50 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int	create_heredoc(char *lim)
 {
-	int	fd;
-	int i;
-	char *line;
+	int		fd;
+	int		i;
+	char	*line;
 
-	fd = open("/tmp/minishell_heredoc.tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644); //change path
+	fd = open("/tmp/minishell_heredoc.tmp", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (!fd)
 		return (-1);
 	i = 1;
 	while (i == 1)
 	{
 		line = readline("heredoc> ");
-		if (ft_strncmp(line, lim, ft_strlen(lim) + 1)) //!= lim->word)
+		if (ft_strncmp(line, lim, ft_strlen(lim) + 1))
 			ft_printf(fd, "%s\n", line);
 		else
 		{
@@ -34,7 +34,6 @@ int	create_heredoc(char *lim)
 	}
 	free(line);
 	close(fd);
-	fd = open("/tmp/minishell_heredoc.tmp", O_RDONLY); // change path
-	close(fd); // necessary ?
+	fd = open("/tmp/minishell_heredoc.tmp", O_RDONLY);
 	return (fd);
 }
